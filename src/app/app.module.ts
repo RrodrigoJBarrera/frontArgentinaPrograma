6b3a71b14cf208365006185b5fe0edb8fb6c50f1
rodrigojbarrera <rbarrera028@gmail.com>
2022-04-06 16:13:45 -0300

    Added Component Presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PresentationComponent } from './components/presentation/presentation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    PresentationComponent,
   ],
   imports: [
     BrowserModule
