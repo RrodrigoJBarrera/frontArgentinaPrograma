a232ffcd00c56010fecca179196bf49cc9ef7bed
rodrigojbarrera <rbarrera028@gmail.com>
2022-07-23 19:38:44 -0300

    heroku

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,8 @@ import { educacion } from '../model/educacion.model';
   providedIn: 'root',
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/';
+  // URL = 'http://localhost:8080/';
+  URL = 'https://still-reef-69263.herokuapp.com/';
   constructor(private http: HttpClient) {}
 
   public obtenerEducacion(): Observable<educacion> {
